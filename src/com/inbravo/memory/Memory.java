1dc464ae2f975c70dd73288ab99e34b9d6a847a8
Amit Dixit <amtdxt@yahoo.com>
2015-07-20 17:50:45 +0530

    RMI example added

---
+++ src/com/inbravo/memory/Memory.java
@@ -0,0 +1,21 @@
+package com.inbravo.memory;
+
+import javax.management.*;
+import java.lang.management.*;
+
+/**
+ * 
+ * @author amit.dixit
+ *
+ */
+public final class Memory {
+
+	public static long threadAllocatedBytes() {
+		try {
+			return (Long) ManagementFactory.getPlatformMBeanServer().invoke(new ObjectName(ManagementFactory.THREAD_MXBEAN_NAME),
+					"getThreadAllocatedBytes", new Object[] { Thread.currentThread().getId() }, new String[] { long.class.getName() });
+		} catch (Exception e) {
+			throw new IllegalArgumentException(e);
+		}
+	}
+}
