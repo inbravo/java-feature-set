436d82ca8222394a48fbfabdf0765194e065f7ff
Amit Dixit <amtdxt@yahoo.com>
2016-01-28 15:58:33 +0530

    New Spark Example added
    New Spark Example added

--- src/com/inbravo/string/IdentityHashCode.java
+++ src/com/inbravo/string/IdentityHashCode.java
@@ -1,5 +1,7 @@
 package com.inbravo.string;
 
+import java.math.BigInteger;
+
 /**
  * 
  * @author amit.dixit
@@ -17,5 +19,16 @@ public final class IdentityHashCode {
 		/* Has code will be same */
 		System.out.println(System.identityHashCode("1"));
 		System.out.println(System.identityHashCode("1"));
+
+		System.out.println(IdentityHashCode.toHex("amit"));
+	}
+
+	/**
+	 * 
+	 * @param arg
+	 * @return
+	 */
+	private static final String toHex(final String arg) {
+		return String.format("%040x", new BigInteger(1, arg.getBytes(/* YOUR_CHARSET? */)));
 	}
 }
