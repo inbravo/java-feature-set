808b7cb9f58df4ba3deccd1adf9dd5dc6df7db32
Amit Dixit <amtdxt@yahoo.com>
2016-02-03 16:29:47 +0530

    JDK 8 stream example

---
+++ src/com/inbravo/stream/ListStreams.java
@@ -0,0 +1,20 @@
+package com.inbravo.stream;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 
+ * @author amit.dixit
+ *
+ */
+public final class ListStreams {
+
+	public static final void main(final String... args) {
+
+		final List<String> myList = Arrays.asList("a1", "a2", "b1", "c2", "c1");
+
+		/* Print only those strings which starts with char 'c' in sorted order */
+		myList.stream().filter(s -> s.startsWith("c")).map(String::toUpperCase).sorted().forEach(System.out::println);
+	}
+}
