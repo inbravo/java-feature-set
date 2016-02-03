5c83efe09d79039ae03d41e1a6ad97afa27732b1
Amit Dixit <amtdxt@yahoo.com>
2016-02-03 18:21:15 +0530

    Build file updated

--- src/com/inbravo/spark/UserTransactionCount.java
+++ src/com/inbravo/spark/UserTransactionCount.java
@@ -27,8 +27,8 @@ public final class UserTransactionCount {
 
 		final JavaSparkContext sc = new JavaSparkContext(new SparkConf().setAppName("UserTransactionCount").setMaster("local"));
 
-		/* Process input files */
-		final JavaPairRDD<String, String> outputRDD = UserTransactionCount.getUserTransactions(sc, args[0], args[1]);
+		/* Process input files and calculate transaction count of users */
+		final JavaPairRDD<String, String> outputRDD = UserTransactionCount.getUserTransactionCount(sc, args[0], args[1]);
 
 		/* Save output on HDFS */
 		outputRDD.saveAsHadoopFile(args[2], String.class, String.class, TextOutputFormat.class);
@@ -38,13 +38,13 @@ public final class UserTransactionCount {
 	}
 
 	@SuppressWarnings("serial")
-	public static final JavaPairRDD<String, String> getUserTransactions(final JavaSparkContext sc, final String transaction,
-			final String user) {
+	private static final JavaPairRDD<String, String> getUserTransactionCount(final JavaSparkContext sc, final String transactionFilePath,
+			final String userFilePath) {
 
 		/* Step 1: Read transactions data */
-		final JavaRDD<String> transactionInputFile = sc.textFile(transaction);
+		final JavaRDD<String> transactionInputFile = sc.textFile(transactionFilePath);
 
-		/* Step 2: Transaform transactions data into key-value pairs */
+		/* Step 2: Transaform transactions data into key (user-id) and value (product-id) pairs */
 		final JavaPairRDD<Integer, Integer> transactionPairs = transactionInputFile.mapToPair(new PairFunction<String, Integer, Integer>() {
 
 			public Tuple2<Integer, Integer> call(final String transaction) {
@@ -52,12 +52,13 @@ public final class UserTransactionCount {
 				System.out.println("Transaction : " + transaction);
 				final String[] transactionSplit = transaction.split("\t");
 
+				/* Tuple : key (user-id) : value (product-id) */
 				return new Tuple2<Integer, Integer>(Integer.valueOf(transactionSplit[2]), Integer.valueOf(transactionSplit[1]));
 			}
 		});
 
 		/* Step 3: Read user data */
-		final JavaRDD<String> userInputFile = sc.textFile(user);
+		final JavaRDD<String> userInputFile = sc.textFile(userFilePath);
 
 		/* Step 4: Transaform users data into key-value pairs */
 		final JavaPairRDD<Integer, String> userPairs = userInputFile.mapToPair(new PairFunction<String, Integer, String>() {
@@ -65,20 +66,27 @@ public final class UserTransactionCount {
 			public Tuple2<Integer, String> call(final String user) {
 
 				System.out.println("User : " + user);
-				final String[] customerSplit = user.split("\t");
+				final String[] userSplit = user.split("\t");
 
-				return new Tuple2<Integer, String>(Integer.valueOf(customerSplit[0]), customerSplit[3]);
+				/* Tuple : key (user-id) : value (country) */
+				return new Tuple2<Integer, String>(Integer.valueOf(userSplit[0]), userSplit[3]);
 			}
 		});
 
 		/* Step 5: Apply join on users and transactions */
 		final JavaRDD<Tuple2<Integer, Optional<String>>> userTransactions = joinData(transactionPairs, userPairs);
 
+		System.out.println("All transactions of user : " + userTransactions);
+
 		/* Step 6: Modify data, convert to key-value pairs */
 		final JavaPairRDD<Integer, String> userTransactionsModified = modifyData(userTransactions);
 
+		System.out.println("Modified transactions of user : " + userTransactionsModified);
+
 		/* Step 7: Count the result */
 		final Map<Integer, Object> result = countData(userTransactionsModified);
+
+		System.out.println("Count of transactions of user : " + result);
 
 		final List<Tuple2<String, String>> output = new ArrayList<>();
 
@@ -106,6 +114,7 @@ public final class UserTransactionCount {
 
 		/* Left Outer Join of transactions on users */
 		final JavaRDD<Tuple2<Integer, Optional<String>>> leftJoinOutput = transactions.leftOuterJoin(users).values().distinct();
+
 		return leftJoinOutput;
 	}
 
