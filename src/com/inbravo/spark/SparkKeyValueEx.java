a36ed4164e91771e4cacd13f9e0e650f101a39af
Amit Dixit <amtdxt@yahoo.com>
2016-02-08 20:04:07 +0530

    JDK 8 settings added
    JDK 8 settings added

--- src/com/inbravo/spark/SparkKeyValueEx.java
+++ src/com/inbravo/spark/SparkKeyValueEx.java
@@ -3,10 +3,8 @@ package com.inbravo.spark;
 import java.util.Arrays;
 
 import org.apache.spark.SparkConf;
-import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 
 /**
@@ -25,8 +23,8 @@ public final class SparkKeyValueEx {
 		final JavaRDD<Integer> rddOne = jsc.parallelize(Arrays.asList(1, 2, 3, 3, 4));
 		final JavaRDD<Integer> rddTwo = jsc.parallelize(Arrays.asList(3, 4, 4, 5, 6));
 
-		System.out.println("========================================================================");
-		System.out.println("---------Various Transformation on RDD " + rddOne.collect() + "} & RDD {" + rddTwo.collect() + "}---------");
+		System.out.println("=========================================================================================================");
+		System.out.println("---------Various Trans/actions on RDD " + rddOne.collect() + "} & RDD {" + rddTwo.collect() + "}---------");
 
 		/* Apply union */
 		final JavaRDD<Integer> unionResult = rddOne.union(rddTwo);
@@ -34,73 +32,42 @@ public final class SparkKeyValueEx {
 		/* Show result */
 		System.out.println("----------Union : " + unionResult.collect());
 
-		/* Apply intersection */
-		final JavaRDD<Integer> intersectionResult = rddOne.intersection(rddTwo);
-
-		/* Show result */
-		System.out.println("---Intersection : " + intersectionResult.collect());
+		/* Show intersection */
+		System.out.println("---Intersection : " + rddOne.intersection(rddTwo).collect());
 
 		/* Apply subtract */
-		final JavaRDD<Integer> subtractResult = rddOne.subtract(rddTwo);
-
-		/* Show result */
-		System.out.println("-------Subtract : " + subtractResult.collect());
+		System.out.println("-------Subtract : " + rddOne.subtract(rddTwo).collect());
 
 		/* Apply cartesian */
-		final JavaPairRDD<Integer, Integer> cartesianResult = rddOne.cartesian(rddTwo);
-
-		/* Show result */
-		System.out.println("------Cartesian : " + cartesianResult.collect());
-
-		System.out.println("-----------------------------------------------------------------------");
-		System.out.println("---------Various Action on RDD " + rddOne.collect() + "} & RDD {" + rddTwo.collect() + "}---------");
+		System.out.println("------Cartesian : " + rddOne.cartesian(rddTwo).collect());
 
 		/* Get all elements */
 		System.out.println("-------Collect : " + rddOne.collect());
 
-		/* Count all elements */
+		/* Count all elements in RDDs */
 		System.out.println("---------Count : " + (rddOne.count() + rddTwo.count()));
 
-		/* Count all values */
+		/* Count all element values */
 		System.out.println("--CountByValue : " + rddOne.countByValue());
 
 		/* Get third element */
 		System.out.println("----------Take : " + rddOne.take(3));
 
-		/* Filter some element */
-		System.out.println("------Filter : " + rddOne.filter(x -> x.intValue() > 4));
+		/* Get only values which are less than 3 */
+		System.out.println("--------Filter : " + rddOne.filter(x -> x.intValue() < 3).collect());
 
-		/* Apply reduce */
-		final Integer reduceResult = rddOne.reduce(reduce());
+		/* Show Reduce result */
+		System.out.println("--------Reduce : " + rddOne.reduce(reduce()));
 
-		/* Show result */
-		System.out.println("--------Reduce : " + reduceResult.intValue());
+		/* Show Map result */
+		System.out.println("-----------Map : " + rddOne.map(x -> x * x).collect());
 
-		/* Apply Map */
-		final JavaRDD<Integer> squareResult = rddOne.map(square());
-
-		/* Show result */
-		System.out.println("-----------Map : " + squareResult.collect());
-		System.out.println("========================================================================");
+		/* FlatMap returns list */
+		System.out.println("-------FlatMap : " + rddOne.flatMap(x -> Arrays.asList(x * x)).collect());
+		System.out.println("=========================================================================================================");
 
 		/* Close spark context */
 		jsc.close();
-	}
-
-	@SuppressWarnings("serial")
-	private static final Function<Integer, Integer> square() {
-
-		/**
-		 * Return Square value
-		 */
-		return new Function<Integer, Integer>() {
-
-			@Override
-			public final Integer call(final Integer value) throws Exception {
-
-				return value * value;
-			}
-		};
 	}
 
 	@SuppressWarnings("serial")
