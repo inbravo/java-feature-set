3edba88ed1f88ee600a51d4d666b6c0eb884118d
Amit Dixit <amtdxt@yahoo.com>
2016-03-03 15:23:07 +0530

    HexTest++

--- src/com/inbravo/puzzle/JoyOfHex.java
+++ src/com/inbravo/puzzle/JoyOfHex.java
@@ -9,6 +9,10 @@ public final class JoyOfHex {
 
 	public static final void main(final String... args) {
 
+		/* Avoid mixed type conputations; '0xcafebabe' is not a long */
 		System.out.println(Long.toHexString(0x100000000L + 0xcafebabe));
+
+		/* Both operands are long now */
+		System.out.println(Long.toHexString(0x100000000L + 0xcafebabeL));
 	}
 }
