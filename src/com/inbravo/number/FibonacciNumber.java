2898eede070b1c8c476a35e621b61a3da94161ac
Amit Dixit <amtdxt@yahoo.com>
2016-04-20 14:03:30 +0530

    fibonacci++

---
+++ src/com/inbravo/number/FibonacciNumber.java
@@ -0,0 +1,29 @@
+package com.inbravo.number;
+
+/**
+ * 
+ * @author amit.dixit
+ *
+ */
+public final class FibonacciNumber {
+
+  public static final void main(String... args) {
+
+    /* Convert the user argument to int */
+    final int i = Integer.valueOf(args[0]);
+
+    /* Find its fibonacci */
+    System.out.println(fibonacci(i));
+  }
+
+  private static final int fibonacci(final int i) {
+
+    if (i <= 0) {
+      return 0;
+    } else if (i == 1) {
+      return 1;
+    } else {
+      return (fibonacci(i - 1) + fibonacci(i - 2));
+    }
+  }
+}
