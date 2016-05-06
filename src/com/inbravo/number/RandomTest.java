e2d6053bc66be44db3ed080decfa41a8c7ea89ca
Amit Dixit <amtdxt@yahoo.com>
2016-05-06 19:20:28 +0530

    RandomTest++
    RandomTest++

--- src/com/inbravo/number/RandomTest.java
+++ src/com/inbravo/number/RandomTest.java
@@ -6,7 +6,7 @@ public final class RandomTest {
     System.out.println("Main not required to print this");
     System.exit(0);
   }
-  
+
   static final int MAX_SEGMENTS = 1 << 16;
 
   public static final void main(final String... args) {
