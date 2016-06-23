f7aa71722b94e4feb3c0a352a1b5e1624e049011
Amit Dixit <amtdxt@yahoo.com>
2016-06-23 19:47:03 +0530

    PredicateTest++

--- src/com/inbravo/jdk8/functions/PredicateTest.java
+++ src/com/inbravo/jdk8/functions/PredicateTest.java
@@ -14,6 +14,18 @@ public final class PredicateTest {
 
   public static final void main(final String... args) {
 
+    /* Create a predicate to check if string is empty */
+    final Predicate<String> stringNotEmpty = PredicateTest::isApple;
+
+    /* Create a list of numberss */
+    final List<String> strings = Arrays.asList("apple", "grape", "mango", "lichi", "");
+
+    /* Filter all null strings */
+    final List<String> filteredString = strings.stream().filter(stringNotEmpty).collect(Collectors.<String>toList());
+
+    /* Print the list */
+    System.out.println(filteredString);
+
     /* Create a predicate to return true only for numbers greater than 5 */
     final Predicate<Integer> atLeast5 = (x) -> x > 5;
 
@@ -25,5 +37,10 @@ public final class PredicateTest {
 
     /* Print the list */
     System.out.println(filteredIntegers);
+  }
+
+  /* Serves the predicate */
+  public static boolean isApple(final String fruit) {
+    return "apple".equals(fruit);
   }
 }
